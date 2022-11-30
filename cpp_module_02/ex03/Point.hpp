0680071d4295ccf41303f00a2e93d279aaaa2d96
Adrian Perol hurtado <aperol-h@c3r2s4.42madrid.com>
2022-11-30 01:25:10 +0100

    .

--- cpp_module_02/ex03/Point.hpp
+++ cpp_module_02/ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: aperol-h <aperol-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 23:49:57 by aperol-h          #+#    #+#             */
-/*   Updated: 2022/11/30 01:07:04 by aperol-h         ###   ########.fr       */
+/*   Updated: 2022/11/30 01:23:02 by aperol-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,6 @@ public:
     const Fixed getY() const;
 };
 
-bool bsp(const Point p0, const Point p1, const Point p2, const Point p);
+bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 #endif
--- cpp_module_02/ex03/bsp.cpp
+++ cpp_module_02/ex03/bsp.cpp
@@ -6,19 +6,19 @@
 /*   By: aperol-h <aperol-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 00:04:27 by aperol-h          #+#    #+#             */
-/*   Updated: 2022/11/30 00:58:46 by aperol-h         ###   ########.fr       */
+/*   Updated: 2022/11/30 01:23:07 by aperol-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool bsp(const Point p0, const Point p1, const Point p2, const Point p) {
-	Fixed s = (p0.getX() - p2.getX()) * (p.getY() - p2.getY()) - (p0.getY() - p2.getY()) * (p.getX() - p2.getX());
-    Fixed t = (p1.getX() - p0.getX()) * (p.getY() - p0.getY()) - (p1.getY() - p0.getY()) * (p.getX() - p0.getX());
+bool bsp( Point const a, Point const b, Point const c, Point const point) {
+	Fixed s = (a.getX() - c.getX()) * (point.getY() - c.getY()) - (a.getY() - c.getY()) * (point.getX() - c.getX());
+    Fixed t = (b.getX() - a.getX()) * (point.getY() - a.getY()) - (b.getY() - a.getY()) * (point.getX() - a.getX());
 
     if ((s < 0) != (t < 0) && s != 0 && t != 0)
         return false;
 
-    Fixed d = (p2.getX() - p1.getX()) * (p.getY() - p1.getY()) - (p2.getY() - p1.getY()) * (p.getX() - p1.getX());
+    Fixed d = (c.getX() - b.getX()) * (point.getY() - b.getY()) - (c.getY() - b.getY()) * (point.getX() - b.getX());
     return d != 0 && (d < 0) == (s + t <= 0);
 }
