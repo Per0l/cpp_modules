b7eeb9d35f3401a9dd73e987807001d30bbb3fca
Adrian Perol hurtado <aperol-h@c3r9s1.42madrid.com>
2022-12-19 21:46:02 +0100

    cpp07 iter fix

--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -6,15 +6,15 @@
 /*   By: aperol-h <aperol-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:24:04 by aperol-h          #+#    #+#             */
-/*   Updated: 2022/11/22 18:45:04 by aperol-h         ###   ########.fr       */
+/*   Updated: 2022/12/19 21:44:22 by aperol-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template<typename T, typename S, typename F>
-void iter(T array, S size, F f) {
+template<typename T, typename S, typename F, typename R>
+void iter(T *array, S size, R (*f)(F)) {
 	if (!array || !f)
 		return;
 	for (S i = 0; i < size; i++)
