f452866200fe2e22c8ebd824cf7c828a76ff9139
Adrian Perol hurtado <aperol-h@c3r9s1.42madrid.com>
2022-12-19 22:50:41 +0100

    .

--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -6,15 +6,23 @@
 /*   By: aperol-h <aperol-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:24:04 by aperol-h          #+#    #+#             */
-/*   Updated: 2022/12/19 21:44:22 by aperol-h         ###   ########.fr       */
+/*   Updated: 2022/12/19 22:29:06 by aperol-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template<typename T, typename S, typename F, typename R>
-void iter(T *array, S size, R (*f)(F)) {
+template<typename T, typename S, typename V>
+void iter(T *array, S size, void(*f)(V)) {
+	if (!array || !f)
+		return;
+	for (S i = 0; i < size; i++)
+		f(array[i]);
+}
+
+template<typename T, typename S>
+void iter(T *array, S size, void(*f)(T const &)) {
 	if (!array || !f)
 		return;
 	for (S i = 0; i < size; i++)
